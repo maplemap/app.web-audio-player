f4dd5ca6defce470e387ccdf1d13d9912a1c6f48
Sergey.Illarionov <easthunch@gmail.com>
2016-02-16 18:20:30 +0200

    fix: redesigned project

--- src/js/helpers/tmpEngine.js
+++ src/js/helpers/tmpEngine.js
@@ -15,17 +15,6 @@ var TmpEngine = (function () {
         Templates = {
             player: function (options) {
                 return '<div id="'+ settings.playerID +'">\
-                            <div class="'+ settings.classPrefix +'-header">\
-                                <span class="'+ settings.classPrefix +'-logo"></span>\
-                            </div>\
-                            <div class="'+ settings.classPrefix +'-information">\
-                                <div class="'+ settings.classPrefix +'-album-cover">\
-                                    <img class="cover-image active" src="https://upload.wikimedia.org/wikipedia/en/d/df/Calvin_Harris_-_18_Months.png" alt="Calvin_Harris_-_18_Months" />\
-                                </div>\
-                                <div class="'+ settings.classPrefix +'-track-name">We’ll be coming back</div>\
-                                <div class="'+ settings.classPrefix +'-author">Calvin Harris</div>\
-                                <div class="'+ settings.classPrefix +'-album-name">18 months</div>\
-                            </div>\
                             <div class="'+ settings.classPrefix +'-playlist">\
                                  <ul class="'+ settings.classPrefix +'-tracker">\
                                     <li>\
@@ -62,16 +51,6 @@ var TmpEngine = (function () {
                                     </li>\
                                  </ul>\
                             </div>\
-                            <ul class="'+ settings.classPrefix +'-controls">\
-                               <li class="prev"></li>\
-                               <li class="play-pause"></li>\
-                               <li class="next"></li>\
-                               <li class="timeline"></li>\
-                               <li class="volume" title="volume"></li>\
-                               <li class="shuffle"></li>\
-                               <li class="repeat"></li>\
-                            </ul>\
-                            <div class="'+ settings.classPrefix +'-footer"> </div>\
                         </div>'
             },
 
@@ -97,3 +76,26 @@ var TmpEngine = (function () {
     }
 
 }());
+
+//<div class="'+ settings.classPrefix +'-header">\
+//<span class="'+ settings.classPrefix +'-logo"></span>\
+//</div>\
+//<div class="'+ settings.classPrefix +'-information">\
+//<div class="'+ settings.classPrefix +'-album-cover">\
+//<img class="cover-image active" src="https://upload.wikimedia.org/wikipedia/en/d/df/Calvin_Harris_-_18_Months.png" alt="Calvin_Harris_-_18_Months" />\
+//</div>\
+//<div class="'+ settings.classPrefix +'-track-name">We’ll be coming back</div>\
+//<div class="'+ settings.classPrefix +'-author">Calvin Harris</div>\
+//<div class="'+ settings.classPrefix +'-album-name">18 months</div>\
+//</div>\
+
+//<ul class="'+ settings.classPrefix +'-controls">\
+//<li class="prev"></li>\
+//<li class="play-pause"></li>\
+//<li class="next"></li>\
+//<li class="timeline"></li>\
+//<li class="volume" title="volume"></li>\
+//<li class="shuffle"></li>\
+//<li class="repeat"></li>\
+//</ul>\
+//<div class="'+ settings.classPrefix +'-footer"> </div>\
