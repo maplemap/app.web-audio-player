1df59423cb8d0a258131edb24604d571c651f3d5
Sergey.Illarionov <easthunch@gmail.com>
2016-02-28 13:23:00 +0300

    add: getFilesDromServer module

--- src/js/modules/getFilesFromServer.js
+++ src/js/modules/getFilesFromServer.js
@@ -1,13 +1,50 @@
+'use strict';
 
+var FilesDownload = (function () {
+    var settings = {
+        urlServer: 'http://php-file-server.local/index.php'
+    },
+    $playlist = false,
+    $fileList = false,
 
+    init = function ($player) {
+        $playlist = $player.find('.playlist');
+        $playlist.append( );
 
-$('.upload-files').on('click', function () {
-    $.ajax({
-        type: "GET",
-        url: "http://php-file-server.local/index.php",
-        cache: false,
-        success: function(result){
-            console.log(result)
-        }
-    });
-})
+        $fileList = $playlist.find('.' + settings.fileListClass);
+
+        initEvents();
+    },
+
+    initEvents = function () {
+        $('.get-files').on('click', function () {
+            var $modalWindow = $playlist.find('.modal-window');
+
+            $modalWindow.toggleClass('active');
+
+            if($modalWindow.hasClass('active')) getFilesList();
+        });
+
+        $fileList.find('.cancel').on('click', function () {
+            $fileList.removeClass('active');
+        });
+    },
+
+    getFilesList = function () {
+        $.ajax({
+            type: "GET",
+            url: settings.urlServer,
+            cache: false,
+            beforeSend: function () {
+
+            },
+            success: function(result){
+                console.log(result)
+            }
+        });
+    };
+
+    return {
+        init: init
+    }
+})();
