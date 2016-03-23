092153a60ec8a1f237d1305f0b8ed23501d05d85
Sergey.Illarionov <easthunch@gmail.com>
2016-03-23 14:36:09 +0200

    add: ids for upload files

--- src/js/models/file.js
+++ src/js/models/file.js
@@ -2,6 +2,12 @@
 
 App.Models.File = Backbone.Model.extend({
     defaults: {
+        index: '',
         filename: ''
+    },
+
+    initialize: function () {
+        this.set('index', App.Helper.counter);
+        App.Helper.counter += 1;
     }
 });
