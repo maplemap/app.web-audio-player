219ab5d4a46f0498fda61f27550557870f12ab39
Sergey.Illarionov <easthunch@gmail.com>
2016-04-01 11:10:38 +0300

    add: getTimeFromSeconds method

--- src/js/collections/track.js
+++ src/js/collections/track.js
@@ -6,23 +6,35 @@ App.Collections.Tracks = Backbone.Collection.extend({
     localStorage: new Backbone.LocalStorage('web-player'),
 
     getTotalTime: function () {
-        var totalSeconds = 0,
-            hours, minutes, seconds, time;
+        var totalSeconds = 0;
 
         this.each(function (obj, index) {
             var timeArray = obj.get('duration').split(':');
             totalSeconds += parseInt(timeArray[0], 10) * 60 + parseInt(timeArray[1], 10);
         });
 
-        hours   = Math.floor(totalSeconds / 3600);
-        minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
-        seconds = totalSeconds - (hours * 3600) - (minutes * 60);
 
-        if (hours   < 10) {hours   = '0' + hours;}
+        return this.getTimeFromSeconds(totalSeconds, 'hours');
+    },
+
+    getTimeFromSeconds: function (seconds, flag) {
+        var hours, minutes, time;
+
+        hours   = Math.floor(seconds / 3600);
+        minutes = Math.floor((seconds - (hours * 3600)) / 60);
+        seconds = seconds - (hours * 3600) - (minutes * 60);
+
+        hours = (hours && hours < 10) ? '0' + hours + ':' : '';
+
+        if (hours) {
+            if (hours < 10) hours =  '0' + hours + ':';
+        } else {
+            hours = (flag === 'hours') ? '00:' : '';
+        }
         if (minutes < 10) {minutes = '0' + minutes;}
         if (seconds < 10) {seconds = '0' + seconds;}
 
-        time = hours + ':' + minutes + ':' + seconds;
+        time = hours + minutes + ':' + seconds;
 
         return time;
     },
